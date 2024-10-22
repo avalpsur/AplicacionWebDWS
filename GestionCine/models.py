84ec7bdbe389a7f7bd53e4991c822380154feb83
Alvaro Valero <alvaro.valero.garcia.al@iespoligonosur.org>
2024-10-22 12:23:17 +0200

    Corrección

--- GestionCine/models.py
+++ GestionCine/models.py
@@ -3,20 +3,22 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
+from django.db import models
+
 class Cliente(models.Model):
-    dni = models.CharField(max_length=9,unique=True)
+    dni = models.CharField(max_length=9, unique=True)
     nombre = models.CharField(max_length=150)
     apellidos = models.CharField(max_length=300)
-    correoElectronico = models.EmailField
+    email = models.EmailField(default='nombre@ejemplo.com')
 
 class Socio(models.Model):
-    numSocio = models.CharField(max_length=5,unique = True)
-    fechaAlta = models.DateField
-    fechaCaducidad = models.DateField
-    cliente = models.OneToOneField(Cliente,on_delete=models.CASCADE)
+    numSocio = models.CharField(max_length=5, unique=True)
+    fechaAlta = models.DateField(default=timezone.now)
+    fechaCaducidad = models.DateField(default=timezone.now)
+    cliente = models.OneToOneField(Cliente, on_delete=models.CASCADE)
 
 class Gerente(models.Model):
-    dni = models.CharField(max_length=9,unique=True)
+    dni = models.CharField(max_length=9, unique=True)
     nombre = models.CharField(max_length=150)
     apellidos = models.CharField(max_length=300)
     telefono = models.CharField(max_length=9)
@@ -24,83 +26,68 @@ class Gerente(models.Model):
 class Cine(models.Model):
     direccion = models.CharField(max_length=500)
     telefono = models.CharField(max_length=9)
-    email = models.EmailField
-
-    gerente = models.OneToOneField(Gerente,on_delete=models.CASCADE)
-
+    email = models.EmailField(default='nombre@ejemplo.com')
+    gerente = models.OneToOneField(Gerente, on_delete=models.CASCADE)
 
 class Empleado(models.Model):
-    dni = models.CharField(max_length=9,unique=True)
+    dni = models.CharField(max_length=9, unique=True)
     nombre = models.CharField(max_length=150)
     apellidos = models.CharField(max_length=300)
-    nuss = models.CharField(max_length=12,unique=True)
-    iban = models.CharField(max_length=24,unique=True)
-    salario = models.IntegerField
+    nuss = models.CharField(max_length=12, unique=True)
+    iban = models.CharField(max_length=24, unique=True)
+    salario = models.IntegerField(default=1100)
     encargado = models.BooleanField(default=False)
-    
-    cine = models.ForeignKey(Cine,on_delete = models.CASCADE)
-
-
+    cine = models.ForeignKey(Cine, on_delete=models.CASCADE)
 
 class Sala(models.Model):
     TAMANO = [
-        ("PE","Pequena"),
-        ("ME","Mediana"),
-        ("GR","Grande"),
+        ("PE", "Pequena"),
+        ("ME", "Mediana"),
+        ("GR", "Grande"),
     ]
-
-
     tamano = models.CharField(
         max_length=2,
         choices=TAMANO,
         default='ME',
     )
-
-    cine = models.ForeignKey(Cine,on_delete = models.CASCADE)
+    cine = models.ForeignKey(Cine, on_delete=models.CASCADE)
     empleado = models.ManyToManyField(Empleado)
-
 
 class Pelicula(models.Model):
     titulo = models.CharField(max_length=500)
     director = models.CharField(max_length=300)
-    sinopsis = models.TextField
-    fechaLanzamiento = models.DateField
-    tiempoProyectada = models.DurationField
+    sinopsis = models.TextField(null=True)
+    fechaLanzamiento = models.DateField(default=timezone.now)
+    tiempoProyectada = models.DurationField(null=True)
     sala = models.ManyToManyField(Sala)
 
-
 class Proyeccion(models.Model):
-    hora = models.DateTimeField
-    sala = models.ForeignKey(Sala,on_delete=models.CASCADE)
+    hora = models.DateTimeField(default=timezone.now)
+    sala = models.ForeignKey(Sala, on_delete=models.CASCADE)
     pelicula = models.ForeignKey(Pelicula, on_delete=models.CASCADE)
-    
-
 
 class Entrada(models.Model):
-    fechaCompra = models.DateTimeField
-    cliente = models.OneToOneField(Cliente,on_delete=models.DO_NOTHING)
-    proyeccion = models.ForeignKey(Proyeccion,on_delete=models.CASCADE)
-
+    fechaCompra = models.DateTimeField(default=timezone.now)
+    cliente = models.OneToOneField(Cliente, on_delete=models.DO_NOTHING)
+    proyeccion = models.ForeignKey(Proyeccion, on_delete=models.CASCADE)
 
 class Proveedor(models.Model):
     nombre = models.CharField(max_length=200)
     direccion = models.CharField(max_length=500)
     telefono = models.CharField(max_length=9)
-    web = models.URLField
-
+    web = models.URLField(null=True)
 
 class Producto(models.Model):
     TIPO = [
-        ("co","comida"),
-        ("be","bebida"),
+        ("co", "comida"),
+        ("be", "bebida"),
     ]
-    
     nombre = models.CharField(max_length=150)
     tipo = models.CharField(
         max_length=2,
-        choices = TIPO,
-        default = 'co',
+        choices=TIPO,
+        default='co',
     )
-    fechaCaducidad = models.DateField
-    proveedor = models.ForeignKey(Proveedor,on_delete=models.CASCADE)
+    fechaCaducidad = models.DateField(default=timezone.now)
+    proveedor = models.ForeignKey(Proveedor, on_delete=models.CASCADE)
     cliente = models.ManyToManyField(Cliente)
