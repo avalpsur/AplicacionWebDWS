76ef860750694fdc3466a1f97dc30c8bd8a2b734
Alvaro Valero <alvaro.valero.garcia.al@iespoligonosur.org>
2024-11-21 17:26:44 +0100

    70% completada

--- miweb/settings.py
+++ miweb/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-w@eb-#9udsu77$r!5r&0&4ix1s^zen)9ttnc*+yk)bsm_aa4bb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com','0.0.0.0']
 
